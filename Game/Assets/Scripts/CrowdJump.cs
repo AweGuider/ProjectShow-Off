67f8a4f63d83850f0a6aea6cc8e78850e0e80a7f
AweGuider <aweguider@gmail.com>
2023-06-25 15:38:29 +0200

    Ensured firecat working, added crowd jumping on delay

--- Game/Assets/Scripts/CrowdJump.cs
+++ Game/Assets/Scripts/CrowdJump.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class JumpController : MonoBehaviour
+public class CrowdJump : MonoBehaviour
 {
     [SerializeField] [Range(0, 1)] float jumpDelay; 
     [HideInInspector] public float minJumpDelay = 1f;
@@ -13,6 +13,7 @@ public class JumpController : MonoBehaviour
 
     private void Start()
     {
+        jumpDelay = Random.Range(0f, 1f);
         animator = GetComponent<Animator>();
         StartCoroutine(JumpDelay());
         //CalculateNextJumpTime();
