89f231732dd0bd36f428ac6a5c98a095b589e0df
AweGuider <aweguider@gmail.com>
2023-06-27 14:27:39 +0200

    Added checkpoint system, adjusted 2P Selection Scene,

---
+++ Game/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out IPlayer player))
+        {
+            player.ReactToDead();
+        }
+    }
+}
