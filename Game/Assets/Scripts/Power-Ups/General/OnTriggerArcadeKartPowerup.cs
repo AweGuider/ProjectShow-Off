781e060a148b3c14bc93dec6f6ffbb6aef9e3919
TheRealMrPepper <chivutudor@gmail.com>
2023-06-29 11:47:07 +0200

    Fixed jellypath slow effect

--- Game/Assets/Scripts/Power-Ups/General/OnTriggerArcadeKartPowerup.cs
+++ Game/Assets/Scripts/Power-Ups/General/OnTriggerArcadeKartPowerup.cs
@@ -4,46 +4,46 @@ using UnityEngine.Events;
 
 public class OnTriggerArcadeKartPowerup : ArcadeKartPowerup
 {
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log($"COLLIDED {other.gameObject.name}");
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    Debug.Log($"COLLIDED {other.gameObject.name}");
 
-        if (IsCoolingDown) return;
+    //    if (IsCoolingDown) return;
 
-        if (other.gameObject.TryGetComponent(out IKart kartTrigger))
-        {
-            KartTrigger trigger = (KartTrigger) kartTrigger;
-            kartTrigger.ReactToJellyPath(boostStats);
-            //ApplyPowerUps(trigger.Kart);
-            Debug.Log($"COLLIDED {other.gameObject.name}");
+    //    if (other.gameObject.TryGetComponent(out IKart kartTrigger))
+    //    {
+    //        KartTrigger trigger = (KartTrigger) kartTrigger;
+    //        kartTrigger.ReactToJellyPath(boostStats);
+    //        //ApplyPowerUps(trigger.Kart);
+    //        Debug.Log($"COLLIDED {other.gameObject.name}");
 
-        }
+    //    }
 
-        //var rb = other.attachedRigidbody;
+    //    //var rb = other.attachedRigidbody;
 
-        //Debug.Log($"Rigibody name: {rb.name}");
-        //if (rb) {
+    //    //Debug.Log($"Rigibody name: {rb.name}");
+    //    //if (rb) {
 
-        //    var kart = rb.GetComponent<ArcadeKart>();
-        //    Debug.Log($"Kart name: {kart.name}");
+    //    //    var kart = rb.GetComponent<ArcadeKart>();
+    //    //    Debug.Log($"Kart name: {kart.name}");
 
-        //    if (kart)
-        //    {
-        //        ApplyPowerUps(kart);
-        //    }
-        //}
-    }
+    //    //    if (kart)
+    //    //    {
+    //    //        ApplyPowerUps(kart);
+    //    //    }
+    //    //}
+    //}
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"TRIGGERED {other.gameObject.name}");
 
         if (IsCoolingDown) return;
 
-        if (other.gameObject.TryGetComponent(out IKart kartTrigger))
+        if (other.gameObject.TryGetComponent(out IKartTrigger kartTrigger))
         {
             KartTrigger trigger = (KartTrigger) kartTrigger;
-            kartTrigger.ReactToJellyPath(boostStats);
-            //ApplyPowerUps(trigger.Kart);
+            //kartTrigger.ReactToJellyPath(boostStats);
+            ApplyPowerUps(trigger.Kart);
             Debug.Log($"TRIGGERED {other.gameObject.name}");
 
         }
